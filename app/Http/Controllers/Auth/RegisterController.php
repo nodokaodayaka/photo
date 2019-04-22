c39b570404b6844edb8a1d315b13d0d3568fea42
nodokaodayaka <wankosob@gmail.com>
2019-04-22 18:14:04 +0900

    mobu day2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,9 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
+use Illuminate\Http\Request; // ★ 追加
+
+
 class RegisterController extends Controller
 {
     /*
@@ -68,5 +71,11 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+
+    // ★ メソッド追加
+    protected function registered(Request $request, $user)
+    {
+        return $user;
     }
 }
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,11 @@
-
+import './bootstrap'
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+// require('./bootstrap');
 
 window.Vue = require('vue');
 
@@ -27,7 +27,23 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+import store from './store'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
 
-const app = new Vue({
-    el: '#app'
-});
+const createApp = async () => {
+    await store.dispatch('auth/currentUser')
+
+    new Vue({
+        el: '#app',
+        router,
+        store,
+        components: { App },
+        template: '<App />'
+    })
+}
+
+createApp()
